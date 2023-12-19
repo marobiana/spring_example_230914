bb87a9a04dee255dbf0a98044f24fd1ce93c896e
DESKTOP-B0NSHI7\user <test@test.com>
2023-12-19 18:43:04 +0900

    update 예제

--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.domain.Review;
@@ -9,7 +10,31 @@ public interface ReviewMapper {
 
 	// input:id    output:Review
 	public Review selectReview(int id);
+	
+	// input:Review        output: 성공한 행의 개수(int)
+	public int insertReview(Review review);
+	
+	public int insertReviewAsField(
+			// 하나의 map 된 것 => @Param
+			@Param("storeId") int storeId111, 
+			@Param("menu") String menu111, 
+			@Param("userName") String userName111, 
+			@Param("point") Double point111, 
+			@Param("review") String review111);
+	
+	// input:id, review     output: 성공한 행의 개수
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review11111111);
 }
+
+
+
+
+
+
+
+
 
 
 
